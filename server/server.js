0e40aec38016f314220ef621bf1e87696e4d7bbb
陳吳柏俊 <pcchenwu@chenwubjundeMBP.lan>
2017-07-21 10:39:09 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to chat room',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New User join!',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
       console.log('createMessage', message);
       io.emit('newMessage', {
           from: message.from,
           text: message.text,
           createdAt: new Date().getTime()
-      })
+      });
+    //   socket.broadcast.emit('newMessage', {
+    //       from: message.from,
+    //       text: message.text,
+    //       createdAt: new Date().getTime()
+    //   });
     });
 
     socket.on('disconnect', () => {
