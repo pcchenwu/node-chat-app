e04aef8d4b3ed88a4c3117c774beed6d3eff60ee
陳吳柏俊 <pcchenwu@chenwubjundeMBP.lan>
2017-07-27 11:25:42 +0800

    change welcom text and userlist text

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ io.on('connection', (socket) => {
       users.addUser(socket.id, params.name, params.room);
 
       io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-      socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+      socket.emit('newMessage', generateMessage('Admin', 'Welcome to Greed Island!'));
       socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin', `${params.name} joined`));
       callback();
     });
